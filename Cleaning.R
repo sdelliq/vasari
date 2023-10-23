9f38b6eee7fd9c55396502e74c94a1cdcd619cd2
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-23 16:43:28 +0200

    Cleaning and calling tables

--- Cleaning.R
+++ Cleaning.R
@@ -4,5 +4,46 @@ original.borrowers.individual <- original.borrowers.individual %>% mutate_all(to
 original.posizioni <- original.posizioni %>% mutate_all(tolower)
 original.anagrafiche <- original.anagrafiche %>% mutate_all(tolower)
 
+#Cleaning the column names
 colnames(original.posizioni) <- clean_column_names(colnames(original.posizioni))
 colnames(original.anagrafiche) <- clean_column_names(colnames(original.anagrafiche))
+
+#Trim
+original.borrowers.corporate <- original.borrowers.corporate %>% mutate_all(str_trim)
+original.borrowers.individual <- original.borrowers.individual %>% mutate_all(str_trim)
+original.posizioni <- original.posizioni %>% mutate_all(str_trim)
+original.anagrafiche <- original.anagrafiche %>% mutate_all(str_trim)
+
+#
+anagrafiche <- original.anagrafiche
+anagrafiche <- anagrafiche %>% mutate(intestaz.unica.rag.soc = gsub("\\s+", " ", intestaz.unica.rag.soc))
+anagrafiche <- anagrafiche %>%
+  mutate(
+    cf.piva = ifelse(
+      !is.na(partita.iva.aziende) & is.na(codice.fiscale.persona.fisica), partita.iva.aziende,
+      ifelse(!is.na(codice.fiscale.persona.fisica) & is.na(partita.iva.aziende), codice.fiscale.persona.fisica,
+             ifelse(forma.giuridica %in% c("ind", "prv"), codice.fiscale.persona.fisica, partita.iva.aziende)
+      )
+    )
+  )
+anagrafiche <- anagrafiche %>% rename(name = intestaz.unica.rag.soc, id.bor = ndg)
+
+anagrafiche <- anagrafiche %>%
+  mutate(across(c(starts_with("nome_"), "name"), ~ gsub("[,.*]+$", "", .))) #Deletes the *.* from the names
+anagrafiche <- anagrafiche %>% mutate_all(str_trim)
+anagrafiche$nome_1 <- gsub("\\s+", " ", anagrafiche$nome_1)
+anagrafiche <- anagrafiche %>%
+  mutate(
+    name = ifelse(
+      is.na(nome_1),
+      name,
+      paste0(
+        ifelse(!is.na(nome_1), nome_1, ""),
+        ifelse(!is.na(nome_2), paste0(", ", nome_2), ""),
+        ifelse(!is.na(nome_3), paste0(", ", nome_3), ""),
+        ifelse(!is.na(nome_4), paste0(", ", nome_4), ""),
+        ifelse(!is.na(nome_5), paste0(", ", nome_5), "")
+      )
+    )
+  )
+
--- Main.R
+++ Main.R
@@ -5,8 +5,11 @@ source("Functions.R")
 original.borrowers.corporate <- read.xlsx("data/DD_EARLY REPORT 62 POS. 13072023.xlsx")
 original.borrowers.individual <- read_excel("data/DD_PDL CON RESIDENZA + IVA - 199 POS. 12072023.xlsx")
 
-data_frames <- read_specific_sheets_and_save_df("data/Vasari_LDT.xlsx", c("Posizioni", "Anagrafiche"))
+data_frames <- read_specific_sheets_and_save_df("data/Vasari_LDT.xlsx", c("Posizioni", "Anagrafiche", "Garanzie"))
 original.posizioni <- data_frames$Posizioni
 original.anagrafiche <- data_frames$Anagrafiche
+original.garanzie <- data_frames$Garanzie
 
 source("Cleaning.R")
+source("tables/Counterparties.R")
+source("tables/Entities.R")
