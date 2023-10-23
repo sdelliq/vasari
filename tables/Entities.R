3e4b7af990a1a306452c9117a5f57a0d13484466
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-23 17:56:29 +0200

    joined with geo

--- tables/Entities.R
+++ tables/Entities.R
@@ -1,4 +1,4 @@
-colnames(anagrafiche)
+
 entities <- anagrafiche %>% select(name, cf.piva, type.subject=forma.giuridica, city=comune, province=provincia, data.di.nascita)
 #id
 entities$id.entity <- paste0("e", seq_len(nrow(counterparties))) 
@@ -8,9 +8,7 @@ entities <- entities %>% mutate(dummy.info=NA,
                                 income.pf=NA,
                                 date.cessation=NA,
                                 status.pg=NA,
-                                flag.imputed=NA,
-                                region=NA,
-                                area = NA)
+                                flag.imputed=NA)
 
 entities <- entities %>% mutate(age = as.numeric(format(Sys.Date(), "%Y")) - as.numeric(format(data.di.nascita, "%Y")))
 entities <- add_age_range_column(entities)
@@ -30,6 +28,19 @@ entities <- entities %>% mutate(
 entities <- add_sex_column(entities)
 
 entities <- add_type.pg_column(entities)
+
+paths_content <- readLines(".paths.txt")
+geoMetadataPath_line <- grep("^geoMetadataPath=", paths_content)
+geoMetadataPath_value <- sub("^geoMetadataPath=\\s*", "", paths_content[geoMetadataPath_line])
+
+GEO.metadata <- read_excel(geoMetadataPath_value, sheet = "Geo")
+GEO.metadata <- GEO.metadata %>% mutate_all(tolower)
+
+entities <- entities %>% rename("or.province" =province)
+
+# Merge specific columns from 'city_info' into 'ENTITIES'
+entities <- left_join(entities, GEO.metadata %>% select(city, province, region, area), by = "city")
+entities <- entities %>% distinct()
 
 
 entities <- entities %>% select(id.entity, name, cf.piva, type.subject, dummy.info, sex, range.age, age, solvency.pf, income.pf, type.pg, status.pg, date.cessation, city, province, region, area, flag.imputed)
