67792bc44cccc16ed146efda120584f6f85ddd95
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-24 14:42:35 +0200

    linking table

---
+++ tables/link.counterparties.entities.R
@@ -0,0 +1,5 @@
+link.counterparties.entities <- counterparties %>% select (id.counterparty, id.bor) 
+link.counterparties.entities <- link.counterparties.entities %>% 
+  left_join(entities %>% select(id.bor, id.entity) %>% divide_column_by_character(., id.bor, ","), by= "id.bor")  %>% select(-id.bor)
+
+entities <- entities %>% select(-id.bor)
