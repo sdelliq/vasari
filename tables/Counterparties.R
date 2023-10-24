aad55ec68a95cb4c040bacfbbd5a0212d6ee99e0
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-24 10:06:34 +0200

    borrowers and guarantors separated

--- tables/Counterparties.R
+++ tables/Counterparties.R
@@ -2,8 +2,7 @@
 
 #useful columns
 counterparties <- anagrafiche %>% select(id.bor, name, tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante)
-#id
-counterparties$id.counterparty <- paste0("c", seq_len(nrow(counterparties))) 
+ 
 #creation of other columns
 count_entities <- function(x) as.integer(str_count(x, ',') + 1)
 counterparties <- counterparties %>% mutate (id.group = NA,
@@ -11,5 +10,15 @@ counterparties <- counterparties %>% mutate (id.group = NA,
                                                              tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante == "g" ~  "guarantor",
                                                              tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante == "dg" ~  "both"),
                                              flag.imputed=NA,
-                                             n.entities = count_entities(name)) %>% 
-  select(-tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante)
+                                             n.entities = count_entities(name))
+
+g <- counterparties %>% filter(role == "both") %>% mutate(role= "guarantor")
+counterparties <- counterparties %>% mutate(role= case_when(role == "both"  ~ "borrower",
+                                                            role == "borrower"  ~ "borrower",
+                                                            role == "guarantor" ~  "guarantor"))
+counterparties <- counterparties %>% rbind(g)
+
+#id
+counterparties$id.counterparty <- paste0("c", seq_len(nrow(counterparties)))
+
+counterparties <- counterparties %>% select(id.counterparty, id.bor, id.group, role, name, n.entities, flag.imputed)
