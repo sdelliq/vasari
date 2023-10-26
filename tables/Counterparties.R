acfb269f11d0a76df22d5d136d822e3b777c7861
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-26 09:17:46 +0200

    adding the n.entities column to counterparties

--- tables/Counterparties.R
+++ tables/Counterparties.R
@@ -4,13 +4,11 @@
 counterparties <- anagrafiche %>% select(id.bor, name, tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante)
  
 #creation of other columns
-count_entities <- function(x) as.integer(str_count(x, ',') + 1)
 counterparties <- counterparties %>% mutate (id.group = NA,
                                              role= case_when(tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante == "d" ~ "borrower",
                                                              tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante == "g" ~  "guarantor",
                                                              tipo.anagrafica.d.debitore.g.garante.dg.debitore.e.garante == "dg" ~  "both"),
-                                             flag.imputed=NA,
-                                             n.entities = count_entities(name))
+                                             flag.imputed=NA)
 
 g <- counterparties %>% filter(role == "both") %>% mutate(role= "guarantor")
 counterparties <- counterparties %>% mutate(role= case_when(role == "both"  ~ "borrower",
@@ -21,4 +19,4 @@ counterparties <- counterparties %>% rbind(g)
 #id
 counterparties$id.counterparty <- paste0("c", seq_len(nrow(counterparties)))
 
-counterparties <- counterparties %>% select(id.counterparty, id.bor, id.group, role, name, n.entities, flag.imputed)
+counterparties <- counterparties %>% select(id.counterparty, id.bor, id.group, role, name, flag.imputed)
