ef6f1585e9209ed72c9846b391a36f82fcc546b9
Eleonora Zarrilli <eleonora.zarrilli@iqera.it>
2023-10-31 09:25:05 +0100

    graphs modified

--- reports/graphs.R
+++ reports/graphs.R
@@ -93,10 +93,10 @@ ggsave("File/Pie_Chart.png",plot = area_plot)
 province_plot <- Top_5_province_by_gbv[2:6,] %>% mutate(`province` = fct_reorder(`province`,`sum.gbv`)) %>% 
   ggplot(aes(x = `sum.gbv` , y = `province`)) +
   geom_col(fill = "#57c1ef", alpha = 0.6, width = 0.4) +
-  geom_text(aes(label = sprintf("%.1fM", `sum.gbv` / 1e6)), hjust = -0.1,size = 3) +
+  geom_text(aes(label = sprintf("%.1fM", `sum.gbv` / 1e6)), hjust = 1.1,size = 3) +
   xlab("Sum of GBV (Millions)") +
   theme_bw() +
-  #xlim(0,16) +
+  #xlim(0,16*1e6) +
   scale_x_continuous(labels = scales::comma_format(scale = 1e-6, big.mark = ","))+
   theme(
     panel.border = element_blank(),
@@ -120,10 +120,18 @@ corporate.status.plot <- corporate.status[2:8,] %>% mutate(`corporate status` = 
   ggplot(aes(x =`corporate status` , y = `%.gbv`)) +
   geom_col(fill = "#57c1ef", alpha = 0.6, width = 0.4) +
   geom_text(aes(label = sprintf("%.1f%%", `%.gbv`*100), group = `corporate status`, y = `%.gbv`/2 , vjust = 0.5,hjust = "center"),size = 3) +
-  geom_text(aes(label = sprintf("%.1fM", `sum.gbv` / 1e6),vjust = -0.4, hjust = 0.4),size = 3) +
+  geom_text(aes(label = sprintf("%.1fM", `sum.gbv` / 1e6),vjust = -0.7, hjust = 0.5),size = 3) +
   xlab("Corporate's Status") +
-  theme_bw() + theme(axis.text.x= element_text(angle=20)) +
-  scale_y_continuous(labels = scales::percent_format(scale = 100,accuracy = 10))
+  theme_bw() + theme(axis.text.x= element_text(angle=10)) +
+  scale_y_continuous(labels = scales::percent_format(scale = 100,accuracy = 10)) +
+  theme(
+    panel.border = element_blank(),
+    panel.grid.minor.x = element_line(),
+    panel.grid.major.y = element_line(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    axis.line = element_line(color = "black")
+  )
 
 
 corporate.status.plot
